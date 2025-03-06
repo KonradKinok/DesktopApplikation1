a6001267095a385fbb887245557e4fb0b6284942
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2025-03-06 17:44:55 +0100

    05.03.2025

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    window.electron.subscribeStatistics((stats) => console.log(stats));
+  }, []);
 
   return (
     <>
@@ -23,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Kliknij na the Vite and React logos to learn more
       </p>
     </>
   );
